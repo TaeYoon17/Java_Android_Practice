348e5f67b73dde051a517774b9d34d81ee6e1db7
TaeYoon17 <kim0517@hotmail.com>
2025-02-06 16:35:53 +0900

    [Feat] 날씨 데이터 모델링

--- app/src/main/java/com/example/myapplication/data/DataClass.java
+++ app/src/main/java/com/example/myapplication/data/DataClass.java
@@ -3,6 +3,8 @@ package com.example.myapplication.data;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
+
 public class DataClass{
     @Expose
     @SerializedName("name") String name;
@@ -10,3 +12,50 @@ public class DataClass{
     public String getName(){return name;}
     public void setName(String name){this.name = name;}
 }
+
+//{
+//        "coord": {
+//        "lon": 10.99,
+//        "lat": 44.34
+//        },
+//        "weather": [
+//        {
+//        "id": 803,
+//        "main": "Clouds",
+//        "description": "broken clouds",
+//        "icon": "04n"
+//        }
+//        ],
+//        "base": "stations",
+//        "main": {
+//        "temp": 276.8,
+//        "feels_like": 276.8,
+//        "temp_min": 271.6,
+//        "temp_max": 276.8,
+//        "pressure": 1033,
+//        "humidity": 44,
+//        "sea_level": 1033,
+//        "grnd_level": 962
+//        },
+//        "visibility": 10000,
+//        "wind": {
+//        "speed": 0.85,
+//        "deg": 214,
+//        "gust": 0.3
+//        },
+//        "clouds": {
+//        "all": 64
+//        },
+//        "dt": 1738812873,
+//        "sys": {
+//        "type": 1,
+//        "id": 6812,
+//        "country": "IT",
+//        "sunrise": 1738823301,
+//        "sunset": 1738859516
+//        },
+//        "timezone": 3600,
+//        "id": 3163858,
+//        "name": "Zocca",
+//        "cod": 200
+//        }
---
+++ app/src/main/java/com/example/myapplication/data/WeatherData.java
@@ -0,0 +1,83 @@
+package com.example.myapplication.data;
+
+import android.view.ViewDebug;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+public final class WeatherData {
+    @Expose @SerializedName("cood") Coord coord;
+    @Expose @SerializedName("weather") ArrayList<Weather> weather;
+    @Expose @SerializedName("base") String base;
+    @Expose @SerializedName("main") Main main;
+    @Expose @SerializedName("visibility") int visibility;
+    @Expose @SerializedName("wind") Wind wind;
+    @Expose @SerializedName("clouds") Clouds clouds;
+    @Expose @SerializedName("dt") int dt;
+    @Expose @SerializedName("sys") Sys sys;
+    @Expose @SerializedName("timezone") int timezone;
+    @Expose @SerializedName("id") int id;
+    @Expose @SerializedName("name") String name;
+    @Expose @SerializedName("cod") int cod;
+
+
+
+
+
+    static public class Coord{
+        @Expose @SerializedName("lon") Double lon;
+        @Expose @SerializedName("lat") Double lat;
+    }
+    static public class Weather{
+        @Expose @SerializedName("id") int id;
+        @Expose @SerializedName("main") String main;
+        @Expose @SerializedName("description") String description;
+        @Expose @SerializedName("icon") String icon;
+    }
+    static public final class Main{
+        @Expose @SerializedName("temp") double temp;
+        @Expose @SerializedName("feels_like") double feels_like;
+        @Expose @SerializedName("temp_min") double temp_min;
+        @Expose @SerializedName("temp_max") double temp_max;
+        @Expose @SerializedName("pressure") double pressure;
+        @Expose @SerializedName("humidity") double humidity;
+        @Expose @SerializedName("sea_level") int sea_level;
+        @Expose @SerializedName("grnd_level") int grnd_level;
+    }
+    static public final class Wind{
+        @Expose @SerializedName("speed") double speed;
+        @Expose @SerializedName("deg") int deg;
+        @Expose @SerializedName("gust") double gust;
+    }
+    static public final class Clouds {
+        @SerializedName("all")  @Expose
+        private Integer all;
+
+        public Integer getAll() {
+            return all;
+        }
+
+        public void setAll(Integer all) {
+            this.all = all;
+        }
+    }
+    static public final class Sys {
+        @Expose
+        @SerializedName("type")
+        int type;
+        @Expose
+        @SerializedName("id")
+        int id;
+        @Expose
+        @SerializedName("country")
+        String country;
+        @Expose
+        @SerializedName("sunrise")
+        int sunrise;
+        @Expose
+        @SerializedName("sunset")
+        int sunset;
+    }
+}
