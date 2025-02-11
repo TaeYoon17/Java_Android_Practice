38ab6479ea1875c3d71ae2fdd887bb182298bce2
TaeYoon17 <kim0517@hotmail.com>
2025-02-11 16:05:19 +0900

    [Feat] App Bar 세팅

--- app/src/main/java/com/example/myapplication/activites/BottomActivity.java
+++ app/src/main/java/com/example/myapplication/activites/BottomActivity.java
@@ -1,10 +1,14 @@
 package com.example.myapplication.activites;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.myapplication.R;
 
@@ -17,6 +21,23 @@ public class BottomActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_bottom);
         int number = getIntent().getIntExtra("zzz",0);
+        Toolbar toolbar = findViewById(R.id.bottom_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_appbar,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        var id = item.getItemId();
+        if(id == R.id.tab1){
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
