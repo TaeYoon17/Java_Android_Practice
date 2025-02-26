d3276486c57c97724d5ee8ea0d7c1e49f9a77198
TaeYoon17 <kim0517@hotmail.com>
2025-02-26 16:05:02 +0900

    [Feat] 이미지 스크롤

--- app/src/main/java/com/example/myapplication/activites/FirstFrameActivity.java
+++ app/src/main/java/com/example/myapplication/activites/FirstFrameActivity.java
@@ -3,7 +3,10 @@ package com.example.myapplication.activites;
 import static android.view.View.INVISIBLE;
 import static android.view.View.VISIBLE;
 
+import android.annotation.SuppressLint;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -20,7 +23,10 @@ public class FirstFrameActivity extends AppCompatActivity {
     FrameLayout frame1,frame2,frame3;
     ImageView img1,img2,img3;
     Button chBtnImg;
+
+    BitmapDrawable bitmap;
     int idx = 0;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,24 +39,46 @@ public class FirstFrameActivity extends AppCompatActivity {
         img3.setVisibility(INVISIBLE);
 
         Resources resources = getResources();
+        bitmap = (BitmapDrawable) resources.getDrawable(R.drawable.stmmpu);
+        int bitmapWidth = bitmap.getIntrinsicWidth();
+        int bitmapHeight = bitmap.getIntrinsicHeight();
+
+        img1.setImageDrawable(bitmap);
+        img1.getLayoutParams().width = bitmapWidth;
+        img1.getLayoutParams().height = bitmapHeight;
+
 
 
         chBtnImg = findViewById(R.id.chImageBtn);
         chBtnImg.setOnClickListener(V->{
-            int val = (idx++) % 3;
-            if(val == 0){
-                img1.setVisibility(INVISIBLE);
-                img2.setVisibility(VISIBLE);
-                img3.setVisibility(INVISIBLE);
-            }else if(val == 1){
-                img1.setVisibility(INVISIBLE);
-                img2.setVisibility(VISIBLE);
-                img3.setVisibility(INVISIBLE);
-            }else if(val == 2){
-                img1.setVisibility(INVISIBLE);
-                img2.setVisibility(INVISIBLE);
-                img3.setVisibility(VISIBLE);
-            }
+            changeImage();
         });
+    }
+
+    private void changeImage(){
+        Resources res = getResources();
+        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.live_confidence);
+        int bitmapWidth = bitmap.getIntrinsicWidth();
+        int bitmapHeight = bitmap.getIntrinsicHeight();
+        img1.setImageDrawable(bitmap);
+        img1.getLayoutParams().width = bitmapWidth;
+        img1.getLayoutParams().height = bitmapHeight;
+    }
+
+    private void changeImageView(){
+        int val = (idx++) % 3;
+        if(val == 0){
+            img1.setVisibility(INVISIBLE);
+            img2.setVisibility(VISIBLE);
+            img3.setVisibility(INVISIBLE);
+        }else if(val == 1){
+            img1.setVisibility(INVISIBLE);
+            img2.setVisibility(VISIBLE);
+            img3.setVisibility(INVISIBLE);
+        }else if(val == 2){
+            img1.setVisibility(INVISIBLE);
+            img2.setVisibility(INVISIBLE);
+            img3.setVisibility(VISIBLE);
+        }
     }
 }
